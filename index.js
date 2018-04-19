69c59bb0c65b0534cf14786f204e28d41e4266eb
ace-22 <sharmadushyant19@gmail.com>
2018-04-20 01:59:48 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$(".abc").hide("");
+
+$(".button").click(function(){
+	$(".abc").toggle("")
+	
+	$(".container").hide("")
+	$(".q").click(function(){
+		$(".abc").hide("");	
+		$(".container").show("");
+		})
+	$(".qw").click(function(){
+		$(".abc").hide("");	
+		$(".container").show("");
+		})	
+	
+})
+})
